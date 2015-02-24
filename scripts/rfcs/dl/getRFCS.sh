d8d36cba95eac5f6b951ec285b3c91d6185875e4
thillux <theil.markus@gmail.com>
2015-02-24 15:00:03 +0100

    works on FreeBSD

--- scripts/rfcs/dl/getRFCS.sh
+++ scripts/rfcs/dl/getRFCS.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-curl -# http://ftp.uni-erlangen.de/mirrors/ripe.net/rfc/tar/RFC-all.tar.gz | tar -xz
+curl http://ftp.uni-erlangen.de/mirrors/ripe.net/rfc/tar/RFC-all.tar.gz -O
+tar xzf RFC-all.tar.gz
 
 rm -rf a rfc1305.tar pdf  ps  txt
 mkdir -p txt ps pdf
